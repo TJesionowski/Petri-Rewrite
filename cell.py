b7f95cc2d91c599359145db46e063b0f7a26de85
EternalZenith <ethan.a.oquin@protonmail.com>
2018-04-23 20:17:54 -0500

    Final changes before refactor.

--- cell.py
+++ cell.py
@@ -3,20 +3,23 @@ import random
 
 FIELD_SIZE = 1200
 
+
 class Cell:
+    """General class for cells"""
     global FIELD_SIZE
 
     def __init__(self, index, position, mass, cell_type="plant", splitMass=200):
         self.attrib = {"species": cell_type, "split_mass" : splitMass, "metabolism": 50 / splitMass, "vision_multiplier": 1}
         self.index = index
-        self.position = position # X and Y coordinates of cell as list of [x, y]
+        self.position = position  # X and Y coordinates of cell as list of [x, y]
         self.mass = mass
-        self.radius = math.sqrt(self.mass/math.pi) * 5 # Set radius for use in rendering and interacting with other cells and world
+        self.radius = math.sqrt(self.mass / math.pi) * 5  # Set radius for use in rendering and interacting with other cells and world
         self.living = True
         self.id = random.randint(0, 99999999)
         self.target_cell = {"index": -1, "id": 0}
 
     def split(self, cell_list, angle):
+        """Creates a new cell of the same type with a portion of the parent cell's mass"""
         if self.attrib["species"] == "herbivore":
             new_position = [(self.position[0] + (math.cos(math.radians(angle)) * (self.radius * 2))), (self.position[1] + (math.sin(math.radians(angle)) * (self.radius * 2)))]
         else:
@@ -25,45 +28,44 @@ class Cell:
         self.mass /= 2
 
     def update(self, cell_list):
-
-        self.mass -= self.mass * 0.001 * self.attrib["metabolism"]# Cells lose one thousandth of their mass per tick
+        """Update the position, mass, and actions of the cell"""
+        self.mass -= self.mass * 0.001 * self.attrib["metabolism"]  # Cells lose one thousandth of their mass per tick
         if self.attrib["species"] == "plant":
-            self.mass += (self.calc_light() / 100) * self.mass # Plant cells gain a certain amount of mass per update cycle, depending on distance from the light source
-            self.radius = math.sqrt(self.mass/math.pi) * 2 # Set radius for use in rendering and interacting with other cells and world
+            self.mass += (self.calc_light() / 100) * self.mass   # Plant cells gain a certain amount of mass per update cycle, depending on distance from the light source
+            self.radius = math.sqrt(self.mass / math.pi) * 2   # Set radius for use in rendering and interacting with other cells and world
         else:
-            self.radius = math.sqrt(self.mass/math.pi) * 3 # Non-plant cells have greater radii per mass
+            self.radius = math.sqrt(self.mass / math.pi) * 3   # Non-plant cells have greater radii per mass
             self.move(self.target(cell_list))
 
-
         # Suffocation and consumption
-        for other in cell_list: # loop through the list of other cells
-            if self.dist(other.position) < (other.radius) and other.id != self.id and other.radius > self.radius: # if the cell is touching this cell and is not this cell, then this cell suffocates
+        for other in cell_list:  # loop through the list of other cells
+            if self.dist(other.position) < (other.radius) and other.id != self.id and other.radius > self.radius:  # if the cell is touching this cell and is not this cell, then this cell suffocates
                 if other.attrib["species"] != "plant":
                     other.mass += self.mass
                 self.living = False
                 break
 
-        if self.mass < self.attrib["split_mass"] / 5: # Check if cell should still be alive
+        if self.mass < self.attrib["split_mass"] / 5:  # Check if cell should still be alive
             self.living = False
-        elif self.mass > self.attrib["split_mass"]: # Check if cell should split
+        elif self.mass > self.attrib["split_mass"]:  # Check if cell should split
             if self.attrib["species"] == "herbivore":
                 self.split(cell_list, self.target(cell_list))
             else:
                 self.split(cell_list, random.randint(0, 360))
 
-    def target(self, cell_list): # Chooses target based on species of cell, returns angle in radians pointing towards chosen target
+    def target(self, cell_list):  # Chooses target based on species of cell, returns angle in radians pointing towards chosen target
         """ Movement, for cells which are capable of it, uses the following decision procedure:
                 Cells check the general attractiveness of areas of the field, with a higher concentration of food making it appear better, and a higher concentration of predators making it appear worse
                 Once it is within what appears to be a good area, it will "see" a target that it should move toward to consume, only ceasing its pursuit if the target is first reached by another cell or a predator nears it and makes it attempt to flee
         """
         direction = 0
-        if self.target_cell["index"] >= 0 and self.target_cell["index"] < len(cell_list) and self.target_cell["id"] == cell_list[self.target_cell["index"]].id: # Make sure that the target from the previous cycle is still alive, and if not, find new target
+        if self.target_cell["index"] >= 0 and self.target_cell["index"] < len(cell_list) and self.target_cell["id"] == cell_list[self.target_cell["index"]].id:  # Make sure that the target from the previous cycle is still alive, and if not, find new target
             direction = math.atan2(
-                (cell_list[self.target_cell["index"]].position[1] - self.position[1]), # Theta = atan2(y, x)
-                (cell_list[self.target_cell["index"]].position[0] - self.position[0])) # Broken into multiplle lines for clarity
+                (cell_list[self.target_cell["index"]].position[1] - self.position[1]),  # Theta = atan2(y, x)
+                (cell_list[self.target_cell["index"]].position[0] - self.position[0]))  # Broken into multiplle lines for clarity
         else:
             distance = 9999
-            for cell in cell_list: # Search for most desirable target, and save it's index and id
+            for cell in cell_list:  # Search for most desirable target, and save it's index and id
                 if cell.mass < self.mass and (cell.attrib["species"] == "plant") and self.dist(cell.position) < distance and not cell.id == self.id:
                     self.target_cell = {"index": cell.index, "id": cell.id}
                     delta_x = cell_list[self.target_cell["index"]].position[1] - self.position[1]
@@ -77,14 +79,12 @@ class Cell:
         """Calculate distance between two points"""
         return math.sqrt(math.pow(self.position[0] - other[0], 2) + math.pow(self.position[1] - other[1], 2))
 
-    def move(self, angle=0, speed=(lambda x: (1 / (math.sqrt(x) / 20)))): # Moves the cell (Note: angle is in radians), speed is function for determining the rate at which movement slows to when mass increases
+    def move(self, angle=0, speed=(lambda x: (1 / (math.sqrt(x) / 20)))):  # Moves the cell (Note: angle is in radians), speed is function for determining the rate at which movement slows to when mass increases
         """ If cell is herbivore, moves towards and consumes plantlike cell, if cell is omnivore, targets nearest and biggest consumable cell of any species
         """
-        self.position = [(self.position[0] + (math.cos(angle) * speed(self.mass) * self.attrib["metabolism"])), (self.position[1] + (math.sin(angle) * speed(self.mass) * self.attrib["metabolism"]))] # Once direction of movement is decided, moves cell in that direction, with speed dependent on metabolic rate and mass
+        self.position = [(self.position[0] + (math.cos(angle) * speed(self.mass) * self.attrib["metabolism"])), (self.position[1] + (math.sin(angle) * speed(self.mass) * self.attrib["metabolism"]))]  # Once direction of movement is decided, moves cell in that direction, with speed dependent on metabolic rate and mass
 
-    def calc_light(self, dispersion_function=(lambda x: (1 /(1 + x / 200)))): # Calculate the amount of light reaching cell; by default the light loses 50% of its strength per 100 pixels
+    def calc_light(self, dispersion_function=(lambda x: (1 / (1 + x / 200)))):  # Calculate the amount of light reaching cell; by default the light loses 50% of its strength per 100 pixels
         """ Calculate amount of light reaching herbivorous cell"""
         distance = self.dist([FIELD_SIZE / 2, FIELD_SIZE / 2])
-        light = dispersion_function(distance) # Light energy falls off with distance, rate depends on function
-
-        return light
+        return dispersion_function(distance)  # Light energy falls off with distance, rate depends on function
--- petri.py
+++ petri.py
@@ -2,7 +2,7 @@ import tkinter
 import random
 from cell import Cell
 
-#Global constants
+# Global constants
 FIELD_SIZE = 1200
 LIGHT_RADIUS = 200
 
@@ -26,29 +26,27 @@ entity_list.append(Cell(len(entity_list), [random.randint(0, 1200), random.randi
 entity_list.append(Cell(len(entity_list), [random.randint(0, 1200), random.randint(0, 1200)], 1000, "herbivore", 250))
 
 
-
 # Update canvas
-def updateField(cell_list):
+def update_field(cell_list):
     """Use previous state of field to generate the next frame"""
     FIELD.delete("all")
 
-    FIELD.create_rectangle(1, 1, FIELD_SIZE, FIELD_SIZE) # Draw border
+    FIELD.create_rectangle(1, 1, FIELD_SIZE, FIELD_SIZE)  # Draw border
 
-    FIELD.create_oval(FIELD_SIZE/2 - LIGHT_RADIUS, FIELD_SIZE/2 - LIGHT_RADIUS, FIELD_SIZE/2 + LIGHT_RADIUS, FIELD_SIZE/2 + LIGHT_RADIUS, fill="yellow") # Draw light source
+    FIELD.create_oval(FIELD_SIZE / 2 - LIGHT_RADIUS, FIELD_SIZE / 2 - LIGHT_RADIUS, FIELD_SIZE / 2 + LIGHT_RADIUS, FIELD_SIZE / 2 + LIGHT_RADIUS, fill="yellow")  # Draw light source
 
-
-    """ Draw Cells from cellList:
-            Iterate over cells in list, getting location and size from cell.position, which returns a two-element list with the current x and y coordinates,
-    then create circle of radius returned by the cell; color according to type
-   """
-    for i in cell_list:
-        size = i.radius
-        x_pos, y_pos = i.position
+    """ Draw Cells from cell_list:
+        Iterate over cells in list, getting location and size from cell.position, which returns a two-element list with the current x and y coordinates,
+        then create circle of radius returned by the cell; color according to type
+    """
+    for entity in cell_list:
+        size = entity.radius
+        x_pos, y_pos = entity.position
 
         # Set color of cell based on species
-        if i.attrib["species"] == "plant":
+        if entity.attrib["species"] == "plant":
             color = "green"
-        elif i.attrib["species"] == "herbivore":
+        elif entity.attrib["species"] == "herbivore":
             color = "blue"
         else:
             color = "red"
@@ -57,11 +55,12 @@ def updateField(cell_list):
 
     CANVAS.update()
 
+
 while True:
-    updateField(entity_list)
-    for i in entity_list:
-        i.update(entity_list)
-        if not i.living:
-            for count in range(i.index, len(entity_list)): # if you remove a cell from the middle of the index, you need to tell the cells after the one you removed that their index has dcreased by one
+    update_field(entity_list)
+    for entity in entity_list:
+        entity.update(entity_list)
+        if not entity.living:
+            for count in range(entity.index, len(entity_list)):  # If you remove a cell from the middle of the index, you need to tell the cells after the one you removed that their index has decreased by one
                 entity_list[count].index -= 1
-            entity_list.remove(i)
+            entity_list.remove(entity)
